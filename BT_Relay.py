3a6c371adcbb69991efc635f25f12cf097953b6d
Dmitry Kuleshov <kuleshovdv@gmail.com>
2016-02-14 19:51:49 +0300

    улучшено

--- BT_Relay.py
+++ BT_Relay.py
@@ -3,22 +3,19 @@ import android
 import hmac
 import hashlib
 
-btAddress = ''
-secret = ''
-
 droid = android.Android()
 
 droid.toggleBluetoothState(True)
 btResult = droid.bluetoothConnect("00001101-0000-1000-8000-00805F9B34FB")
 if btResult.result:
     droid.dialogCreateAlert(u"Enter secret key")
-    droid.dialogCreatePassword(secret)
+    droid.dialogCreatePassword()
     droid.dialogSetPositiveButtonText(u"Open")
     droid.dialogSetNegativeButtonText(u"Cancel")
     droid.dialogShow()
     response = droid.dialogGetResponse()
-    if (response.result['which'] == u'positive'):
-        secret = response.result['value']    
+    if (response.result[u'which'] == u'positive'):
+        secret = str(response.result[u'value'])    
         droid.bluetoothWrite("OPEN\r\n")
         sessionTicket = droid.bluetoothReadLine().result
         #print "Session Ticket: " + sessionTicket
